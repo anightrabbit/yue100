a0ec821451efc8a66e249fe641c4dabaf2426e32
iVan <ivan@iVandeMacBook-Pro.local>
2022-03-28 21:10:52 +0800

    add env config

--- src/request/detail.js
+++ src/request/detail.js
@@ -1,7 +1,19 @@
 import Vue from "vue";
+import qs from "query-string"
 
 const getDetail = (id = 50) => {
-    const url = `api/index.php?s=ydd&c=show&id=${id}&api_call_function=module_show&appid=1&appsecret=PHPCMF9CEAD1B8FFE8B`
+    const query = {
+        s: 'ydd',
+        c: 'show',
+        api_call_function: 'module_show',
+        id,
+        appid: process.env.VUE_APP_ID,
+        appsecret: process.env.VUE_APP_SECRET
+    }
+    const url = qs.stringifyUrl({
+        url: process.env.BASE_URL + process.env.VUE_APP_API_PATH,
+        query,
+    })
     return Vue.http.get(url).then(json => {
         return json.body;
     }, err => {
