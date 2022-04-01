1b6383b63e4a5e0c8f845eaed639d6726f3d8905
iVan <ivan@iVandeMacBook-Pro.local>
2022-04-01 13:17:54 +0800

    publicPath to detail

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const {
   defineConfig
 } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/app/',
+  publicPath: '/detail/',
   transpileDependencies: true,
   devServer: {
     proxy: {
